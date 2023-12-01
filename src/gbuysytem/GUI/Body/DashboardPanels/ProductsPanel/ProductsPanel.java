1ab64865d8045a6a952880f7ebe3c51a9978346c
GroupBuyAdmin <groupbuysystem@gmail.com>
2023-12-01 19:08:15 +0800

    initials

--- src/gbuysytem/GUI/Body/DashboardPanels/ProductsPanel/ProductsPanel.java
+++ src/gbuysytem/GUI/Body/DashboardPanels/ProductsPanel/ProductsPanel.java
@@ -14,15 +14,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -85,15 +76,51 @@ public class ProductsPanel implements PanelReturner{
     }
 
     private void setupButtonPanelBehaviour(JButton addButton, JButton filterButton) {
-
-         
+        
         addButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
-               
+             CreateProductPopUp popUp = new CreateProductPopUp(ProductsPanel.this);
+             popUp.PopUp();    
             }
         });
+
+
+        String url = "jdbc:mysql://localhost:3306/gbuy";
+        String username = "root";
+        String password = "";
+
+        String sqlQuery = "SELECT name, price, quantity, category,details, image FROM product";
+
+        try (
+            // Establishing a connection to the database
+            Connection connection = DriverManager.getConnection(url, username, password);
+            // Creating a PreparedStatement for the SQL query
+            PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
+            // Executing the query and getting the ResultSet
+            ResultSet resultSet = preparedStatement.executeQuery()
+        ) {
+            // Processing the result set     
+            while (resultSet.next()) {
+                String name = resultSet.getString("name");    
+                String cat = resultSet.getString("category");
+                String price = resultSet.getString("price");
+                String qty = resultSet.getString("quantity");
+                String detail = resultSet.getString("details");
+                byte[] imageData = resultSet.getBytes("image");
+    
+                // Convert image data to an Image object
+                ByteArrayInputStream bis = new ByteArrayInputStream(imageData);
+                ImageIcon imageIcon = new ImageIcon(new ImageIcon(imageData).getImage());  
+     
+                Product p = new Product(imageIcon, name,  price, qty,cat, detail);
+                addDashboardItem(p);        
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
 
         filterButton.addActionListener(new ActionListener() {
             @Override
@@ -212,4 +239,5 @@ public class ProductsPanel implements PanelReturner{
 
 
 
- 
+
+
